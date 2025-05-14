492925b09fc80fcbdb4120d5bcafdfc87bc98e22
LuisAdrian5519 <luisadrian.cabrera5519@gmail.com>
2025-05-14 08:58:11 -0700

    Figue_base

--- Software/Machine_Learning_Solutions/Yolov8.py
+++ Software/Machine_Learning_Solutions/Yolov8.py
@@ -8,4 +8,7 @@ model = YOLO("yolov11n.yaml")
 
 # train the model
 
-model.train(data="config.yaml", epochs=15, batch=16)
+# model.train(data="config.yaml", epochs=15, batch=16)
+
+# model.train(data="config.yaml", epochs=10, batch=16, device=0, fraction = 0.30)
+
